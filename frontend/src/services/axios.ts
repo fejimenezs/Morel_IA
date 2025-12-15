c44952ca7b44897854ba963147c62fbf05a4d056
santiago-castilla <santiagojosuecastillacampo@gmail.com>
2025-12-15 16:41:48 -0500

    Use environment variable for API base URL

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // backend Nest
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
